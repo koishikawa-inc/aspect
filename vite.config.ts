fb1f0231e23b4e26cf03ddbff6bbed358ba07fa1
Hiroto Sawada <darowasahito@icloud.com>
2022-11-16 11:19:01 +0900

    Update vite.config.ts
    GitHub Pages用のビルド設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,8 @@ export default defineConfig({
   //     "@/": path.join(__dirname, "./src/"),
   //   },
   // },
+  base: "aspect",
+  build: {
+    outDir: "docs",
+  },
 });
